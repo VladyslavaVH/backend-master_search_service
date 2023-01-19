ad18b0b7a5f4f1ef873a6b1584cdd097369fe85e
G.Vladislava251 <G.Vladislava251@gmail.com>
2023-01-19 17:27:26 +0200

    change verify.js

--- middleware/verify.js
+++ middleware/verify.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 function verifyJWT (req, res, next) {
     const authHeader = req.headers.authorization || req.headers.Authorization;
-    if (!authHeader.startsWith('Bearer ')) return res.sendStatus(401);
+    if (!authHeader && !authHeader.startsWith('Bearer ')) return res.sendStatus(401);
   
     const token = authHeader && authHeader.split(" ")[1];
     if (!token) return res.sendStatus(401);
