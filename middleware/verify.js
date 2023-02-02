80247b566870f7e66713fa4d7f142174e40385b6
G.Vladislava251 <G.Vladislava251@gmail.com>
2023-02-02 16:29:46 +0200

    bug fixed 2

--- middleware/verify.js
+++ middleware/verify.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 function verifyJWT (req, res, next) {
     const authHeader = req.headers.authorization || req.headers.Authorization;
-    if (!authHeader && !authHeader.startsWith('Bearer ')) return res.sendStatus(401);
+    if (!authHeader) return res.sendStatus(401);
     if (authHeader)  {
       if (!authHeader.startsWith('Bearer ')) return res.sendStatus(401);
     }
